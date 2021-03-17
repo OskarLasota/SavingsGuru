468a15a910339eefcc10671dcec13296b729f94a
oskarlasota <frezzyysmite@gmail.com>
2021-03-17 19:30:31 +0000

    chore: setting up navigation component

--- app/src/main/java/com/frezzcoding/savingsguru/MainActivity.kt
+++ app/src/main/java/com/frezzcoding/savingsguru/MainActivity.kt
@@ -2,10 +2,51 @@ package com.frezzcoding.savingsguru
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+
     }
+
+    private fun setupViews(){
+        val navController = Navigation.findNavController(this,
+            R.id.nav_host_fragment
+        )
+        bottom_nav.setOnNavigationItemReselectedListener {
+            //do nothing to ignore reselection of same fragment
+        }
+        setupBottomNavMenu(navController)
+        setSupportActionBar(toolbar)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        bottom_nav?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_nav, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = Navigation.findNavController(this,
+            R.id.nav_host_fragment
+        )
+        val navigated = NavigationUI.onNavDestinationSelected(item, navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
+
 }
---
+++ app/src/main/java/com/frezzcoding/savingsguru/functionalities/home/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.frezzcoding.savingsguru.functionalities.home
+
+import androidx.fragment.app.Fragment
+import com.frezzcoding.savingsguru.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+
+}
