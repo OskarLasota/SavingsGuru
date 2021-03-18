62ebf2d3070cd36ef9566132973c782d5f2752e2
oskarlasota <frezzyysmite@gmail.com>
2021-03-18 16:13:18 +0000

    matching navigation ids to fragments

--- app/src/main/java/com/frezzcoding/savingsguru/functionalities/newscenario/NewScenarioFragment.kt
+++ app/src/main/java/com/frezzcoding/savingsguru/functionalities/newscenario/NewScenarioFragment.kt
@@ -1,9 +1,16 @@
 package com.frezzcoding.savingsguru.functionalities.newscenario
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.frezzcoding.savingsguru.R
 
 class NewScenarioFragment : Fragment(R.layout.fragment_newscenario) {
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //should hide bottom nav bar but show back button
+    }
 
 }
--- app/src/main/java/com/frezzcoding/savingsguru/functionalities/settings/SettingsFragment.kt
+++ app/src/main/java/com/frezzcoding/savingsguru/functionalities/settings/SettingsFragment.kt
@@ -1,7 +1,14 @@
 package com.frezzcoding.savingsguru.functionalities.settings
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.frezzcoding.savingsguru.R
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //should hide bottom nav bar but show back button
+    }
 }
