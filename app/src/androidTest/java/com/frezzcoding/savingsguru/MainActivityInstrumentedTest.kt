17fbe6d753eb02bfba20a4ed818dbcf4dda8f71b
oskarreply <o.lasota@reply.com>
2021-03-22 19:53:24 +0000

    adding espresso tests for navigation

--- app/src/androidTest/java/com/frezzcoding/savingsguru/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/frezzcoding/savingsguru/MainActivityInstrumentedTest.kt
@@ -1,5 +1,12 @@
 package com.frezzcoding.savingsguru
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,11 +19,83 @@ class MainActivityInstrumentedTest {
 
 
     @Test
-    fun switchToSettingsFragment(){
+    fun navigate_To_Settings_Fragment(){
+        //setup
+        ActivityScenario.launch(MainActivity::class.java)
 
+        //nav directorsfragment
+        onView(withId(R.id.settingsFragment)).perform(click())
 
+        //verify
+        onView(withId(R.id.fragment_settings_container))
+            .check(matches(isDisplayed()))
+
+        //on press back
+        pressBack()
+
+        //verify
+        onView(withId(R.id.fragment_home_container))
+            .check(matches(isDisplayed()))
     }
 
 
+    @Test
+    fun navigate_To_Statistics_Fragment() {
+        //setup
+        ActivityScenario.launch(MainActivity::class.java)
+
+        //nav directorsfragment
+        onView(withId(R.id.statsFragment)).perform(click())
+
+        //verify
+        onView(withId(R.id.fragment_statistics_container))
+            .check(matches(isDisplayed()))
+
+        //on press back
+        pressBack()
+
+        //verify
+        onView(withId(R.id.fragment_home_container))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun navigate_To_Graphs_Fragment() {
+        //setup
+        ActivityScenario.launch(MainActivity::class.java)
+
+        //nav directorsfragment
+        onView(withId(R.id.graphsFragment)).perform(click())
+
+        //verify
+        onView(withId(R.id.fragment_graph_container))
+            .check(matches(isDisplayed()))
+
+        //on press back
+        pressBack()
+
+        //verify
+        onView(withId(R.id.fragment_home_container))
+            .check(matches(isDisplayed()))
+    }
+    @Test
+    fun navigate_To_new_scenario_Fragment() {
+        //setup
+        ActivityScenario.launch(MainActivity::class.java)
+
+        //nav directorsfragment
+        onView(withId(R.id.addFragment)).perform(click())
+
+        //verify
+        onView(withId(R.id.fragment_newscenario_container))
+            .check(matches(isDisplayed()))
+
+        //on press back
+        pressBack()
+
+        //verify
+        onView(withId(R.id.fragment_home_container))
+            .check(matches(isDisplayed()))
+    }
 
 }
---
+++ app/src/test/java/com/frezzcoding/savingsguru/MainActivityTest.kt
@@ -0,0 +1,16 @@
+package com.frezzcoding.savingsguru
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class MainActivityTest {
+
+
+    @Test
+    fun test(){
+
+    }
+
+}
