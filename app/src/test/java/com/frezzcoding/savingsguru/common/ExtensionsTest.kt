3b0035d9b400d80abf44b762959eff76c9e5683e
oskarlasota <frezzyysmite@gmail.com>
2021-04-05 12:37:19 +0100

    testing extension to find lowestValue

--- app/src/test/java/com/frezzcoding/savingsguru/common/ExtensionsTest.kt
+++ app/src/test/java/com/frezzcoding/savingsguru/common/ExtensionsTest.kt
@@ -22,4 +22,22 @@ class ExtensionsTest {
         assert(list.highestNumber() == 42)
     }
 
+    @Test
+    fun lowestNumber_list_IsNull(){
+        //when
+        val nullList : List<Int> = listOf()
+
+        //then
+        assert(nullList.lowestNumber() == null)
+    }
+
+    @Test
+    fun lowestNumber_findsLowestValue(){
+        //when
+        val list : List<Int> = listOf(-2, 0, -1, 5, 21, 42, 2)
+
+        //then
+        assert(list.lowestNumber() == -2)
+    }
+
 }
