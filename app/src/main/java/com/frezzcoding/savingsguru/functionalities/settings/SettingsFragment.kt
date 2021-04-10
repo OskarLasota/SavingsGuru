781e3c795cc639cc757e979d5d25718e9b959282
oskarlasota <frezzyysmite@gmail.com>
2021-04-10 17:45:44 +0100

    creating ui for settings fragment and setting up listeners

--- app/src/main/java/com/frezzcoding/savingsguru/functionalities/settings/SettingsFragment.kt
+++ app/src/main/java/com/frezzcoding/savingsguru/functionalities/settings/SettingsFragment.kt
@@ -1,16 +1,54 @@
 package com.frezzcoding.savingsguru.functionalities.settings
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.frezzcoding.savingsguru.R
+import com.frezzcoding.savingsguru.databinding.FragmentSettingsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SettingsFragment : Fragment(R.layout.fragment_settings) {
+class SettingsFragment : Fragment() {
+
+    private lateinit var binding : FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_settings , container, false
+        )
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //should hide bottom nav bar but show back button
+        setListeners()
     }
+
+    private fun setListeners(){
+        binding.switchNotification.setOnCheckedChangeListener { _, isChecked ->
+            //isChecked shows the new checked status
+            if(isChecked){
+                activateNotification()
+            }else{
+                disableNotification()
+            }
+        }
+    }
+
+    private fun activateNotification(){
+
+    }
+
+    private fun disableNotification(){
+
+    }
+
 }
