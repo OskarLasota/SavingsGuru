3b9882f617d2d8382dc199077cd1715909e19795
oskarlasota <frezzyysmite@gmail.com>
2021-04-17 16:32:26 +0100

    chore: creating cardview layout to present scenarios on home screen

--- app/src/main/java/com/frezzcoding/savingsguru/functionalities/home/adapter/ScenarioViewHolder.kt
+++ app/src/main/java/com/frezzcoding/savingsguru/functionalities/home/adapter/ScenarioViewHolder.kt
@@ -3,11 +3,12 @@ package com.frezzcoding.savingsguru.functionalities.home.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.frezzcoding.savingsguru.data.models.Scenario
+import kotlinx.android.synthetic.main.item_scenario.view.*
 
 class ScenarioViewHolder(view : View) : RecyclerView.ViewHolder(view) {
 
     fun bindTo(scenario : Scenario){
-
+        itemView.tv_title.text = scenario.title
     }
 
 }
