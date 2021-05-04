232d49303642d42c64dfae9537c590b150bd5fb3
oskarlasota <frezzyysmite@gmail.com>
2021-05-04 22:04:43 +0100

    setting up foundation for notification broadcast receiver

--- app/src/main/java/com/frezzcoding/savingsguru/common/NotificationManager.kt
+++ app/src/main/java/com/frezzcoding/savingsguru/common/NotificationManager.kt
@@ -1,7 +1,33 @@
 package com.frezzcoding.savingsguru.common
 
-class NotificationManager {
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.frezzcoding.savingsguru.MainActivity
+
+class NotificationManager : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        var builder : NotificationCompat.Builder = NotificationCompat.Builder(context!!, "notification")
+            //.setSmallIcon(R.drawable.transaprent_logo_small)
+            .setContentTitle("Notification title")
+            .setContentText("Context text")
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(PendingIntent.getActivity(context, 0, Intent(context, MainActivity::class.java),0))
+
+        var notificationManager = NotificationManagerCompat.from(context)
+
+        setupNewNotification()
+
+        notificationManager.notify(200, builder.build())
+    }
 
 
+    private fun setupNewNotification(){
 
+
+    }
 }
