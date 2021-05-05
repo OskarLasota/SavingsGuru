3f7e199b2d4186b8c1cb72c86ac8edee539b12ba
oskarlasota <frezzyysmite@gmail.com>
2021-05-05 19:11:11 +0100

    setting up app logo

--- app/src/main/java/com/frezzcoding/savingsguru/common/NotificationManager.kt
+++ app/src/main/java/com/frezzcoding/savingsguru/common/NotificationManager.kt
@@ -7,18 +7,21 @@ import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.frezzcoding.savingsguru.MainActivity
+import com.frezzcoding.savingsguru.R
 
 class NotificationManager : BroadcastReceiver() {
+
+
     override fun onReceive(context: Context?, intent: Intent?) {
-        var builder : NotificationCompat.Builder = NotificationCompat.Builder(context!!, "notification")
-            //.setSmallIcon(R.drawable.transaprent_logo_small)
+        val builder : NotificationCompat.Builder = NotificationCompat.Builder(context!!, "notification")
+            .setSmallIcon(R.drawable.logo)
             .setContentTitle("Notification title")
             .setContentText("Context text")
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(PendingIntent.getActivity(context, 0, Intent(context, MainActivity::class.java),0))
 
-        var notificationManager = NotificationManagerCompat.from(context)
+        val notificationManager = NotificationManagerCompat.from(context)
 
         setupNewNotification()
 
